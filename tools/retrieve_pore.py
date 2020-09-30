aec6f19dcdfc8e5abcb80d7b58219b04b51c776c
James Bockman <james@aiml.team>
2020-09-30 11:31:33 +0930

    Switching machine

---
+++ tools/retrieve_pore.py
@@ -0,0 +1,9 @@
+
+
+# Recieve pore ID from user
+
+# Search pore ID in original image annotation .jsons to find its image
+
+# Search image prediction .json for pore GT and prediction
+
+# Crop pore from image with visualisation overlay (or not?)
