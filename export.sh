838287e7f7794d8101c02f00f3e730bfd5041793
James Bockman <james@aiml.team>
2020-11-30 18:25:54 +1030

    Updated export script

--- export.sh
+++ export.sh
@@ -1,7 +1,6 @@
-python ../detectron2/tools/deploy/caffe2_converter.py \
+python ./tools/caffe2_converter.py \
     --format onnx \
     --config-file configs/mask_rcnn_28x56.yaml \
     --run-eval \
     --output ./output/onnx/mask_rcnn_28x56/ \
-    opts MODEL.WEIGHTS ./arabidopsis_weights.pth \
-    MODEL.ROI_KEYPOINT_HEAD.POOLER_RESOLUTION '(14, 14)'
+    MODEL.WEIGHTS ./arabidopsis_weights.pth
