a77ec4824c731e4b8cf2f0d0116d3885ff58b03a
James Bockman <james@aiml.team>
2021-01-18 09:16:11 +1030

    update

--- demo_arabidopsis.sh
+++ demo_arabidopsis.sh
@@ -1,7 +1,7 @@
 python demo/demo.py \
     --config-file configs/mask_rcnn_arabidopsis_final_v4_cloud.yaml \
-    --output output_demo_arabidopsis \
+    --output output_demo_arabidopsis_streamlit \
     --input datasets/datasets/arabidopsis/human_benchmark \
     --annotations datasets/datasets/arabidopsis/stoma/annotations/val.json \
-    --confidence-threshold 0.5 \
-    --opts MODEL.WEIGHTS ./arabidopsis_v4_weights.pth
+    --confidence-threshold 0.0 \
+    --opts MODEL.WEIGHTS ./arabidopsis_weights.pth
