f2fd9679adc4f3327fb4434eb5c5995f8a75480f
xdynames <james.bockman@live.com.au>
2021-06-14 19:36:25 +0930

    Final

--- demo_arabidopsis.sh
+++ demo_arabidopsis.sh
@@ -1,7 +1,7 @@
 python3 demo/demo.py \
     --config-file configs/mask_rcnn_arabidopsis.yaml \
     --output output_demo_arabidopsis \
-    --input datasets/arabidopsis/stoma/images/ \
+    --input datasets/arabidopsis/stoma/human/ \
     --annotations datasets/arabidopsis/stoma/annotations/GroundTruth.json \
     --confidence-threshold 0.5 \
     --gpu \
